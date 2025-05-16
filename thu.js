bc78d2e92dd862d557d53636bbd182bd54775871
saro2k06 <saravanasaravana29751>
2025-05-16 12:44:43 +0530

    first commit

---
+++ thu.js
@@ -0,0 +1,35 @@
+setTimeout (() =>{
+    document.getElementById("count").innerHTML=10
+    setTimeout (() =>{
+    document.getElementById("count").innerHTML=9
+    setTimeout (() =>{
+    document.getElementById("count").innerHTML=8
+    setTimeout (() =>{
+    document.getElementById("count").innerHTML=7
+    setTimeout (() =>{
+    document.getElementById("count").innerHTML=6
+    setTimeout (() =>{
+    document.getElementById("count").innerHTML=5
+    setTimeout (() =>{
+    document.getElementById("count").innerHTML=4
+    setTimeout (() =>{
+    document.getElementById("count").innerHTML=3
+    setTimeout (() =>{
+    document.getElementById("count").innerHTML=2
+    setTimeout (() =>{
+    document.getElementById("count").innerHTML=1
+    setTimeout (() =>{
+    document.getElementById("count").innerHTML="HAPPY NEW YEAR"
+    console.log("HAPPY NEW YEAR")
+    },2000)
+
+},2000)
+},2000)
+},2000)
+},2000)
+},2000)
+},2000)
+},2000)
+},2000)
+},2000)
+},2000)
